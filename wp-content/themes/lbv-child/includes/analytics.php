8cbfa7351021ea9acff0cf022084fe47ab5e3a15
Loibv <buivanloi.2010@gmail.com>
2026-05-20 16:46:23 +0200

    IT_CM 20/05/2026

--- wp-content/themes/lbv-child/includes/analytics.php
+++ wp-content/themes/lbv-child/includes/analytics.php
@@ -12,7 +12,7 @@ function google_enqueue_scripts(){
 //        );
 
         wp_enqueue_script('analytics',
-            'https://www.googletagmanager.com/gtag/js?id=G-74G4QGDXBG',
+            'https://www.googletagmanager.com/gtag/js?id=G-EZS4GW92SX',
             array(),
             null,
             true
@@ -23,7 +23,7 @@ function google_enqueue_scripts(){
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-74G4QGDXBG');
+gtag('config', 'G-EZS4GW92SX');
 </script>\n";
         }, 20);
     }
